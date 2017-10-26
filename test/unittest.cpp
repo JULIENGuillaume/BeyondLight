fc522ab09031781e265f7d9443171dd434bba13d
guillaume.julien@epitech.eu <guillaume.julien@epitech.eu>
2017-10-27 00:00:55 +0200

    Unit tests

---
+++ test/unittest.cpp
@@ -0,0 +1,16 @@
+/*
+** unittest.cpp for BeyondLight in /home/guillaume/delivery/BeyondLight/unittest.cpp
+**
+** Made by Guillaume Julien
+** Login   <guillaume.julien@epitech.eu>
+**
+** Started on  Thu Oct 26 16:03:23 2017 Guillaume Julien
+** Last update Thu Oct 26 16:03:23 2017 Guillaume Julien
+*/
+
+#include <limits.h>
+#include "gtest/gtest.h"
+
+TEST(FactorialTest, Zero) {
+	EXPECT_EQ(1, 0 + 2);
+}
