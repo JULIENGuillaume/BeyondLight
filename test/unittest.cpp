03d85b5d773d4a881814fb3cf508bf3414792f7f
guillaume.julien@epitech.eu <guillaume.julien@epitech.eu>
2017-10-27 15:48:06 +0200

    Starting architecture, with bette CMakeLists

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -8,9 +8,16 @@
 ** Last update Thu Oct 26 16:03:23 2017 Guillaume Julien
 */
 
-#include <limits.h>
 #include "gtest/gtest.h"
 
-TEST(FactorialTest, Zero) {
-	EXPECT_EQ(1, 0 + 2);
+TEST(SimpleAdd, Diff) {
+	EXPECT_NE(1, 0 + 2);
+}
+
+TEST(SimpleAdd, NoDiff) {
+	EXPECT_EQ(2, 0 + 2);
+}
+
+TEST(ComplexeAdd, NoDiff) {
+	EXPECT_EQ(123, 100 + 20 + 3);
 }
