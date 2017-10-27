d356e5c5e700997ad66a79ed47422f6d26ae1224
guillaume.julien@epitech.eu <guillaume.julien@epitech.eu>
2017-10-27 19:00:58 +0200

    Basic server

---
+++ src/network/server/Server.cpp
@@ -0,0 +1,19 @@
+/*
+** Server.cpp for BeyondLight in /home/guillaume/delivery/BeyondLight/Server.cpp
+**
+** Made by Guillaume Julien
+** Login   <guillaume.julien@epitech.eu>
+**
+** Started on  Fri Oct 27 18:22:22 2017 Guillaume Julien
+** Last update Fri Oct 27 18:22:22 2017 Guillaume Julien
+*/
+
+#include "Server.hh"
+
+bl::network::Server::Server() {
+
+}
+
+bl::network::Server::Server(unsigned short port) {
+
+}
