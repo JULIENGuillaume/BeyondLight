6250efe888e1ce4d5b578e1f8e34718d1e56f5ad
diguie_t <timothy.diguiet@epitech.eu>
2017-11-03 00:31:10 +0100

    removed cmake-build from gitingore, and pushed client cmake-builds

---
+++ client/cmake-build-debug/CMakeFiles/CEFOffScreen.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CEFOffScreen.dir/browser_client.cpp.obj"
+  "CMakeFiles/CEFOffScreen.dir/gl_core.cpp.obj"
+  "CMakeFiles/CEFOffScreen.dir/main.cpp.obj"
+  "CMakeFiles/CEFOffScreen.dir/render_handler.cpp.obj"
+  "CMakeFiles/CEFOffScreen.dir/web_core.cpp.obj"
+  "CEFOffScreen.pdb"
+  "CEFOffScreen.exe"
+  "CEFOffScreen.exe.manifest"
+  "CEFOffScreen.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CEFOffScreen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
