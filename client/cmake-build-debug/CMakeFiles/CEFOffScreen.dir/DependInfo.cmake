cb14908434a2a6ab242136779e52ba73e4628e60
diguie_t <timothy.diguiet@epitech.eu>
2017-11-03 00:31:10 +0100

    removed cmake-build from gitingore, and pushed client cmake-builds

---
+++ client/cmake-build-debug/CMakeFiles/CEFOffScreen.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Desktop/BeyondLight/Git/BeyondLight/client/browser_client.cpp" "E:/Desktop/BeyondLight/Git/BeyondLight/client/cmake-build-debug/CMakeFiles/CEFOffScreen.dir/browser_client.cpp.obj"
+  "E:/Desktop/BeyondLight/Git/BeyondLight/client/gl_core.cpp" "E:/Desktop/BeyondLight/Git/BeyondLight/client/cmake-build-debug/CMakeFiles/CEFOffScreen.dir/gl_core.cpp.obj"
+  "E:/Desktop/BeyondLight/Git/BeyondLight/client/main.cpp" "E:/Desktop/BeyondLight/Git/BeyondLight/client/cmake-build-debug/CMakeFiles/CEFOffScreen.dir/main.cpp.obj"
+  "E:/Desktop/BeyondLight/Git/BeyondLight/client/render_handler.cpp" "E:/Desktop/BeyondLight/Git/BeyondLight/client/cmake-build-debug/CMakeFiles/CEFOffScreen.dir/render_handler.cpp.obj"
+  "E:/Desktop/BeyondLight/Git/BeyondLight/client/web_core.cpp" "E:/Desktop/BeyondLight/Git/BeyondLight/client/cmake-build-debug/CMakeFiles/CEFOffScreen.dir/web_core.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  "GLFW_DLL"
+  "UNICODE"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/cef"
+  "../lib/glm"
+  "../lib/glew/include"
+  "../lib/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
