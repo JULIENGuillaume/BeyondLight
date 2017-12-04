b4adeae1887844d4ee0705d3f963dc14dc5f7d11
Guillaume Julien <guillaume.julien68@gmail.com>
2017-12-04 23:13:33 +0100

    Add JSON library, Serializable interface, and the basic architecture for buildings

---
+++ src/server/game/building/ABuilding.cpp
@@ -0,0 +1,52 @@
+//
+// Created by Guillaume on 04/12/2017.
+//
+
+#include "ABuilding.hh"
+
+server::game::building::ABuilding::ABuilding(int id,
+                                             std::string const &name,
+                                             std::string const &desc,
+                                             std::vector<server::game::resource::UpgradeCost> const &upgrades) : m_id(id),
+                                                                                                                 m_name(name),
+                                                                                                                 m_desc(desc),
+                                                                                                                 m_upgrades(upgrades) {}
+
+nlohmann::json server::game::building::ABuilding::serialize() const {
+	nlohmann::json json;
+
+	json["id"] = m_id;
+	json["level"] = m_level;
+	if (this->getResources().isValid())
+		json["resourcesRequired"] = this->getResources().serialize();
+	json["isUpgrading"] = m_upgrading;
+	json["timeLeft"] = m_upgradeTimeLeft;
+
+	return json;
+}
+
+common::pattern::ISerializable *server::game::building::ABuilding::deserialize(nlohmann::json const &json) {
+	return this;
+}
+
+const std::string &server::game::building::ABuilding::getName() const {
+	return this->m_name;
+}
+
+const std::string &server::game::building::ABuilding::getDescription() const {
+	return this->m_desc;
+}
+
+const server::game::resource::UpgradeCost &server::game::building::ABuilding::getResources() const {
+	if (this->m_level >= this->m_upgrades.size())
+		return server::game::resource::UpgradeCost(false);
+	return this->m_upgrades[this->m_level];
+}
+
+int server::game::building::ABuilding::getId() const {
+	return this->m_id;
+}
+
+int server::game::building::ABuilding::getLevel() const {
+	return this->m_level;
+}
---
+++ src/server/game/technology/ITechnology.hh
@@ -0,0 +1,18 @@
+//
+// Created by Guillaume on 04/12/2017.
+//
+
+#ifndef BEYONDLIGHT_ITECHNOLOGY_HH
+#define BEYONDLIGHT_ITECHNOLOGY_HH
+
+namespace server {
+	namespace game {
+		namespace technology {
+			class ITechnology {
+
+			};
+		}
+	}
+}
+
+#endif //BEYONDLIGHT_ITECHNOLOGY_HH
