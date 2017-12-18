3dfbc3a96ed4530c5ad0cb3ce7a7cb10d6a39095
Guillaume Julien <guillaume.julien68@gmail.com>
2017-12-18 12:30:59 +0100

    Update resources on update building request

--- src/server/game/building/IronMine.cpp
+++ src/server/game/building/IronMine.cpp
@@ -2,15 +2,21 @@
 // Created by Guillaume on 04/12/2017.
 //
 
+#include <iostream>
 #include "../planet/Planet.hh"
 #include "IronMine.hh"
 
 bl::server::game::building::IronMine::IronMine(planet::Planet &planet) :
-		ABuilding(1, "Iron mine", "A mine to retrieve iron from the depth of the earth", {resource::UpgradeCost(true)}, planet) {
+		ABuilding(1, "Iron mine", "A mine to retrieve iron from the depth of the earth", {
+				resource::UpgradeCost(common::game::Resources(), 60),
+				resource::UpgradeCost(common::game::Resources(100), 200),
+				resource::UpgradeCost(common::game::Resources(1000, 50), 800),
+				resource::UpgradeCost(common::game::Resources(3000, 300), 1200)
+		}, planet) {
 }
 
 void bl::server::game::building::IronMine::updateResource() {
-	auto actualTime = this->m_chrono.getElapsedMinutes() + this->m_timeLeftFromLastProd;
+	auto actualTime = this->m_chrono.getElapsedSeconds() + this->m_timeLeftFromLastProd;
 	if (actualTime >= this->m_secondsForProduction) {
 		this->m_planet.getStockResources().addIron((actualTime / this->m_secondsForProduction) * (static_cast<uint64_t>(10 * std::pow(1.42, this->m_level))));
 		this->m_timeLeftFromLastProd = actualTime % this->m_secondsForProduction;
