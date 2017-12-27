d6ddbbc0a9043b165a5d023492d443f326deffe1
Guillaume Julien <guillaume.julien68@gmail.com>
2017-12-28 00:18:14 +0100

    For some reason, crystal extractor disappeared from model handler

--- src/client/mvc/model/ModelHandler.cpp
+++ src/client/mvc/model/ModelHandler.cpp
@@ -11,8 +11,9 @@ namespace bl {
 		ModelHandler::ModelHandler(std::shared_ptr<network::client::ClientNetworkHandler> networkHandler) {
 			this->m_networkHandler = networkHandler;
 			this->m_data = {
-					{"building-iron-mine", std::shared_ptr<ABaseModel>(new BuildingModel(networkHandler, 1))}, // todo "buidling-id" instead ?
-					{"player-resources", std::shared_ptr<ABaseModel>(new ResourcesModel(networkHandler, ::boost::uuids::uuid()))}
+				{"building-iron-mine",         std::shared_ptr<ABaseModel>(new BuildingModel(networkHandler, 1))}, // todo "buidling-id" instead ?
+				{"building-crystal-extractor", std::shared_ptr<ABaseModel>(new BuildingModel(networkHandler, 2))}, // todo "buidling-id" instead ?
+				{"player-resources",           std::shared_ptr<ABaseModel>(new ResourcesModel(networkHandler, ::boost::uuids::uuid()))}
 			};
 		}
 	}
