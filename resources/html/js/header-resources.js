0c8e7f9f19b80dfc5685d56c5fb7931d8147fc10
diguie_t <timothy.diguiet@epitech.eu>
2018-02-06 19:17:06 +0100

    upgrade button are now disabled when not enough resources

--- resources/html/js/header-resources.js
+++ resources/html/js/header-resources.js
@@ -8,6 +8,35 @@ function updateResources(iron, crystal, iridium, antimatter, energy) {
     document.getElementById("energy-value").innerText = energy;
 }
 
+function isUpgradable(iron, crystal, iridium, antimatter, energy) {
+    if (iron > 0) {
+        if (parseInt(document.getElementById("metal-value").innerText, 10) < parseInt(iron, 10)) {
+            return (false);
+        }
+    }
+    if (crystal > 0) {
+        if (parseInt(document.getElementById("crystal-value").innerText, 10) < parseInt(crystal, 10)) {
+            return (false);
+        }
+    }
+    if (iridium > 0) {
+        if (parseInt(document.getElementById("deuterium-value").innerText, 10) < parseInt(iridium, 10)) {
+            return (false);
+        }
+    }
+    if (antimatter > 0) {
+        if (parseInt(document.getElementById("antimatter-value").innerText, 10) < parseInt(antimatter, 10)) {
+            return (false);
+        }
+    }
+    if (energy > 0) {
+        if (parseInt(document.getElementById("energy-value").innerText, 10) < parseInt(energy, 10)) {
+            return (false);
+        }
+    }
+    return (true);
+}
+
 function updateLoop() {
         var request_id = window.cefQuery({
             request: 'update-player-resources',
